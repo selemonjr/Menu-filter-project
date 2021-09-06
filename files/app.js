9dfd4c43e5481dd9e82a6f2e3ee4fe72deffd250
selemonjr <selemonjr@gmail.com>
2021-09-06 02:47:32 -0700

    First Commit

---
+++ files/app.js
@@ -0,0 +1,111 @@
+const menu = [{
+    id : 1,
+    category : "breakfast",
+    price : "$24.50",
+    food : "Coffee",
+    img : "../food-images/breakfast-1.jpg",
+    info : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error sed molestiae recusandae ut veritatis molestias ad vel fuga cumque quos tempora nostrum consequatur laudantium blanditiis nisi eius quibusdam perspiciatis nobis ullam exercitationem quo, corrupti numquam ipsam magnam? Voluptas, optio!Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis in amet animi porro velit omnis aliquid soluta. Tempora nisi, corrupti nostrum veniam tenetur itaque animi, adipisci reiciendis voluptatem dolore voluptatibus!"
+}, {
+    id : 2,
+    category : "breakfast",
+    price : "$14.50",
+    food : "Pancakes",
+    img : "../food-images/breakfast-2.jpg",
+    info : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error sed molestiae recusandae ut veritatis molestias ad vel fuga cumque quos tempora nostrum consequatur laudantium blanditiis nisi eius quibusdam perspiciatis nobis ullam exercitationem quo, corrupti numquam ipsam magnam? Voluptas, optio!Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis in amet animi porro velit omnis aliquid soluta. Tempora nisi, corrupti nostrum veniam tenetur itaque animi, adipisci reiciendis voluptatem dolore voluptatibus!" 
+}, {
+    id : 3,
+    category : "breakfast",
+    price : "$22.50",
+    food : "Oats",
+    img : "../food-images/breakfast-3.jpg",
+    info : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error sed molestiae recusandae ut veritatis molestias ad vel fuga cumque quos tempora nostrum consequatur laudantium blanditiis nisi eius quibusdam perspiciatis nobis ullam exercitationem quo, corrupti numquam ipsam magnam? Voluptas, optio!Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis in amet animi porro velit omnis aliquid soluta. Tempora nisi, corrupti nostrum veniam tenetur itaque animi, adipisci reiciendis voluptatem dolore voluptatibus!"
+},{
+    id : 4,
+    category : "Lunch",
+    price : "$34.50",
+    food : "Chicken Breasts",
+    img : "../food-images/lunch-1.jpg",
+    info : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error sed molestiae recusandae ut veritatis molestias ad vel fuga cumque quos tempora nostrum consequatur laudantium blanditiis nisi eius quibusdam perspiciatis nobis ullam exercitationem quo, corrupti numquam ipsam magnam? Voluptas, optio!Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis in amet animi porro velit omnis aliquid soluta. Tempora nisi, corrupti nostrum veniam tenetur itaque animi, adipisci reiciendis voluptatem dolore voluptatibus!"
+}, {
+    id : 5,
+    category : "lunch",
+    price : "$44.50",
+    food : "Fries and Burgers",
+    img : "../food-images/lunch-2.jpg",
+    info : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error sed molestiae recusandae ut veritatis molestias ad vel fuga cumque quos tempora nostrum consequatur laudantium blanditiis nisi eius quibusdam perspiciatis nobis ullam exercitationem quo, corrupti numquam ipsam magnam? Voluptas, optio!Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis in amet animi porro velit omnis aliquid soluta. Tempora nisi, corrupti nostrum veniam tenetur itaque animi, adipisci reiciendis voluptatem dolore voluptatibus!"
+}, {
+    id : 6,
+    category : "lunch",
+    price : "$7.50",
+    food : "Burgers",
+    img : "../food-images/lunch-3.jpg",
+    info : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error sed molestiae recusandae ut veritatis molestias ad vel fuga cumque quos tempora nostrum consequatur laudantium blanditiis nisi eius quibusdam perspiciatis nobis ullam exercitationem quo, corrupti numquam ipsam magnam? Voluptas, optio!Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis in amet animi porro velit omnis aliquid soluta. Tempora nisi, corrupti nostrum veniam tenetur itaque animi, adipisci reiciendis voluptatem dolore voluptatibus!"
+}, {
+    id : 7,
+    category : "shakes",
+    price : "$3.50",
+    food : "Strawberry",
+    img : "../food-images/shakes-1.jpg",
+    info : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error sed molestiae recusandae ut veritatis molestias ad vel fuga cumque quos tempora nostrum consequatur laudantium blanditiis nisi eius quibusdam perspiciatis nobis ullam exercitationem quo, corrupti numquam ipsam magnam? Voluptas, optio!Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis in amet animi porro velit omnis aliquid soluta. Tempora nisi, corrupti nostrum veniam tenetur itaque animi, adipisci reiciendis voluptatem dolore voluptatibus!"
+}, {
+    id : 8,
+    category : "shakes",
+    price : "$1.50",
+    food : "Chocolate",
+    img : "../food-images/shakes-2.jpg",
+    info : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error sed molestiae recusandae ut veritatis molestias ad vel fuga cumque quos tempora nostrum consequatur laudantium blanditiis nisi eius quibusdam perspiciatis nobis ullam exercitationem quo, corrupti numquam ipsam magnam? Voluptas, optio!Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis in amet animi porro velit omnis aliquid soluta. Tempora nisi, corrupti nostrum veniam tenetur itaque animi, adipisci reiciendis voluptatem dolore voluptatibus!"
+}, {
+    id : 9,
+    category : "shakes",
+    price : "$5.50",
+    food : "Vanilla",
+    img : "../food-images/shakes-3.jpg",
+    info : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error sed molestiae recusandae ut veritatis molestias ad vel fuga cumque quos tempora nostrum consequatur laudantium blanditiis nisi eius quibusdam perspiciatis nobis ullam exercitationem quo, corrupti numquam ipsam magnam? Voluptas, optio!Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis in amet animi porro velit omnis aliquid soluta. Tempora nisi, corrupti nostrum veniam tenetur itaque animi, adipisci reiciendis voluptatem dolore voluptatibus!"
+}, {
+    id : 10,
+    category : "lunch",
+    price : "$9.50",
+    food : "Broccoli",
+    img : "../food-images/lunch-4.jpg",
+    info : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error sed molestiae recusandae ut veritatis molestias ad vel fuga cumque quos tempora nostrum consequatur laudantium blanditiis nisi eius quibusdam perspiciatis nobis ullam exercitationem quo, corrupti numquam ipsam magnam? Voluptas, optio!Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis in amet animi porro velit omnis aliquid soluta. Tempora nisi, corrupti nostrum veniam tenetur itaque animi, adipisci reiciendis voluptatem dolore voluptatibus!"
+}];
+const filterBtn = document.querySelectorAll(".filter-btn");
+const selection = document.querySelector(".selection-center");
+window.addEventListener("DOMContentLoaded",() => {
+    displayMenuItems(menu);
+});
+filterBtn.forEach((btn) => {
+    btn.addEventListener("click",(e) => {
+        const category = e.currentTarget.dataset.id;
+        const categoryMenu = menu.filter((menuItem)=> {
+            if(menuItem.category === category) {
+                return menuItem
+            }
+        });
+        if(category === "all") {
+            displayMenuItems(menu);
+        }
+        else {
+            displayMenuItems(categoryMenu)
+        }
+    });
+});
+function displayMenuItems(menuItems) {
+    let displayMenu = menuItems.map((item) => {
+        return `<div class="item-1">
+        <img src=${item.img} alt="">
+    </div>
+    <div class="item-2">
+        <div class="text">
+            <h3 id="food">${item.food}</h3>
+            <h3 id="price">${item.price}</h3>
+        </div>
+        <span id="line"></span>
+        <div class="information">
+            <p id="info">${item.info}</p>
+        </div>
+    </div>`;
+    });
+    displayMenu = displayMenu.join("");
+    selection.innerHTML = displayMenu
+};
